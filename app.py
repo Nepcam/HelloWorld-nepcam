791707ae51528ea020080d31fd2713a8754e8d76
Cameron Nepe <35090575+Nepcam@users.noreply.github.com>
2022-06-16 20:34:32 +1200

    weight convertor

--- app.py
+++ app.py
@@ -1,3 +1,3 @@
-birth_year = input('Birth year: ')
-age = 2022 - int(birth_year)
-print(age)
+weight_lbs = input('What is your weight in pounds? ')
+weight_kg = float(weight_lbs) * 0.45
+print(int(weight_kg))
