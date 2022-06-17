4ba390bcf95b7d8fad5df4499212c6282b372f99
Cameron Nepe <35090575+Nepcam@users.noreply.github.com>
2022-06-17 23:47:12 +1200

    age calculator

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
+# birth_year = input('Birth year: ')
+# print(type(birth_year))
+# current_year = 2022
+# age = current_year - int(birth_year)
+# print(type(age))
+# print(age)
+
 weight_lbs = input('What is your weight in pounds? ')
 weight_kg = float(weight_lbs) * 0.45
 print(int(weight_kg))
